143b71a0477ad75741e93b67c3436bde863ecc4c
Chneemann <Andre-Ke@gmx.de>
2024-03-23 11:30:46 +0100

    added imprint component

---
+++ src/app/shared/components/imprint/imprint.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss',
+})
+export class ImprintComponent {
+  constructor(private location: Location) {}
+
+  backClicked() {
+    this.location.back();
+  }
+}
--- src/app/shared/components/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/components/privacy-policy/privacy-policy.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [],
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss',
+  styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
-  constructor(private location: Location) {}
 
-  backClicked() {
-    this.location.back();
-  }
 }
