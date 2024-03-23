b83454a7229d0eec7b4a8a5400b87e88bd9019c8
Chneemann <Andre-Ke@gmx.de>
2024-03-23 16:15:55 +0100

    bugfixes & added/design navbar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavbarComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  navbarVisible: boolean = false;
+
+  constructor(private elementRef: ElementRef) {}
+
+  toggleNavbar() {
+    this.navbarVisible = !this.navbarVisible;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    const targetElement = event.target as HTMLElement;
+
+    // Überprüfe, ob der Klick außerhalb der Navbar erfolgt ist
+    if (
+      !targetElement.closest('app-navbar') &&
+      !targetElement.closest('.img-user')
+    ) {
+      this.navbarVisible = false;
+    }
+  }
+}
