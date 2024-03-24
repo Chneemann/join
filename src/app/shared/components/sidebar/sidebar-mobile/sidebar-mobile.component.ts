9d4fe14b3b46f5f65ccd96f285a932a98f0f14c7
Chneemann <Andre-Ke@gmx.de>
2024-03-24 08:30:57 +0100

    added mobile sidebar

--- src/app/shared/components/sidebar/sidebar-mobile/sidebar-mobile.component.ts
+++ src/app/shared/components/sidebar/sidebar-mobile/sidebar-mobile.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-mobile',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar-mobile.component.html',
-  styleUrl: './sidebar-mobile.component.scss'
+  styleUrl: './sidebar-mobile.component.scss',
 })
 export class SidebarMobileComponent {
+  currentPath: string = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.getCurrentPath();
+  }
+
+  getCurrentPath() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentPath = this.router.url.substring(1);
+      }
+    });
+  }
 }
