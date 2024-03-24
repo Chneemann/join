cf508534fa99b31ce4815a22513da1dd30d07449
Chneemann <Andre-Ke@gmx.de>
2024-03-24 13:58:29 +0100

    bugfixes drag & drop

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -15,17 +15,27 @@ export class BoardComponent {
   constructor(
     public dragDropService: DragDropService,
     public firebaseService: FirebaseService
-  ) {
-    this.dragDropService.itemDropped.subscribe(({ index, status }) => {
-      this.firebaseService.allTasks[index].status = status;
-      this.firebaseService.updateTask(
-        this.firebaseService.allTasks[index].id,
-        index
-      );
-    });
-  }
+  ) {}
 
   ngOnInit() {
     this.firebaseService.updateAllTasks();
+    this.dragDropService.itemDropped.subscribe(({ index, status }) => {
+      this.handleItemDropped(index, status);
+    });
+  }
+
+  handleItemDropped(index: number, status: string): void {
+    let firebaseId = this.firebaseService.allTasks[index].id;
+    this.firebaseService.allTasks[index].status = status;
+    this.firebaseService.updateTask(firebaseId, index);
+  }
+
+  isTaskRendered(taskColumn: string): boolean {
+    for (let task of this.firebaseService.allTasks) {
+      if (task.status === taskColumn) {
+        return true;
+      }
+    }
+    return false;
   }
 }
