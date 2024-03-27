d7819f732e0e50fa93cf733c26d58fb5f8c80072
Chneemann <Andre-Ke@gmx.de>
2024-03-27 17:33:15 +0100

    added animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -31,7 +30,7 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     provideHttpClient(),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     importProvidersFrom(
       provideFirebaseApp(() =>
         initializeApp({
