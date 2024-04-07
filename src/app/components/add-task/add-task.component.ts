75b02685ec9c6c17303f3ed70ed32ecf895e8964
Chneemann <Andre-Ke@gmx.de>
2024-04-07 12:18:24 +0200

    added prio btns & save form in local storage

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,19 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss',
 })
 export class AddTaskComponent {
+  currentDate: string = new Date().toISOString().split('T')[0];
+
   taskData = {
     title: '',
     description: '',
-    date: '',
+    date: this.currentDate,
+    priority: 'medium',
   };
+
+  ngOnInit() {
+    const storedTaskData = localStorage.getItem('taskData');
+    if (storedTaskData) {
+      this.taskData = JSON.parse(storedTaskData);
+    }
+  }
+
+  tooglePriority(prio: string) {
+    this.taskData.priority !== prio
+      ? (this.taskData.priority = prio)
+      : this.taskData.priority;
+    this.saveTaskData();
+  }
+
+  saveTaskData() {
+    localStorage.setItem('taskData', JSON.stringify(this.taskData));
+  }
 
   onSubmit(ngForm: NgForm) {
     if (ngForm.submitted && ngForm.form.valid) {
