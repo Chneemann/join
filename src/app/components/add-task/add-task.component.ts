5f8813b9ba750604d9f40bd43f1c3f1c9d8bf593
Chneemann <Andre-Ke@gmx.de>
2024-04-07 11:12:23 +0200

    added title & description input

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.scss'
+  styleUrl: './add-task.component.scss',
 })
 export class AddTaskComponent {
+  taskData = {
+    title: '',
+    description: '',
+  };
 
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid) {
+      console.log('Send completed');
+    }
+  }
 }
