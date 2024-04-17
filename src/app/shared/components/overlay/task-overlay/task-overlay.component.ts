f2273b9534fa8df2c984643a978d6b3649b617f5
Chneemann <Andre-Ke@gmx.de>
2024-04-17 22:52:25 +0200

    added & design date

--- src/app/shared/components/overlay/task-overlay/task-overlay.component.ts
+++ src/app/shared/components/overlay/task-overlay/task-overlay.component.ts
@@ -29,4 +29,27 @@ export class TaskOverlayComponent {
       .getAllTasks()
       .filter((task) => task.id === taskId);
   }
+
+  timeConverter(dateString: string) {
+    var a = new Date(dateString);
+    var months = [
+      'Jan.',
+      'Feb.',
+      'Mar.',
+      'Apr.',
+      'May.',
+      'Jun.',
+      'Jul.',
+      'Aug.',
+      'Sep.',
+      'Oct.',
+      'Nov.',
+      'Dec.',
+    ];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = month + ' ' + date + ', ' + year;
+    return time;
+  }
 }
