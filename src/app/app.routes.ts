5ea171847eda15c7c5bf8be022cb81227d614d56
Chneemann <Andre-Ke@gmx.de>
2024-04-25 08:33:26 +0200

    task overlay responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,13 +7,13 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
 import { BoardComponent } from './components/board/board.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { OverlayComponent } from './shared/components/overlay/overlay.component';
+import { TaskOverlayComponent } from './shared/components/overlay/task-overlay/task-overlay.component';
 
 export const routes: Routes = [
   { path: '', component: SummaryComponent },
   { path: 'summary', component: SummaryComponent },
   { path: 'add-task', component: AddTaskComponent },
   { path: 'board', component: BoardComponent },
-  { path: 'board/:id', component: BoardComponent },
   { path: 'contacts', component: ContactsComponent },
   { path: 'contacts/:id', component: ContactsComponent },
   { path: 'help', component: HelpComponent },
