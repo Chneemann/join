907075e31bdd5b0dc615a15f02bb21b05acb3ce7
Chneemann <Andre-Ke@gmx.de>
2024-04-28 19:09:09 +0200

    responsive

--- src/app/shared/components/imprint/imprint.component.ts
+++ src/app/shared/components/imprint/imprint.component.ts
@@ -1,19 +1,33 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { BtnBackComponent } from '../buttons/btn-back/btn-back.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [TranslateModule, BtnBackComponent],
+  imports: [TranslateModule, BtnBackComponent, CommonModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
-  constructor(private location: Location) {}
+  isRouteLogin: boolean = false;
+
+  constructor(private location: Location, private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkCurrentRoute();
+  }
 
   backClicked() {
     this.location.back();
+  }
+
+  checkCurrentRoute(): void {
+    const currentUrl = this.router.url;
+    if (currentUrl === '/login/imprint') {
+      this.isRouteLogin = true;
+    }
   }
 }
--- src/app/shared/components/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/components/privacy-policy/privacy-policy.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
-import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, Location } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { BtnBackComponent } from '../buttons/btn-back/btn-back.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [TranslateModule, BtnBackComponent],
+  imports: [TranslateModule, BtnBackComponent, CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss',
 })
-export class PrivacyPolicyComponent {
-  constructor(private location: Location) {}
+export class PrivacyPolicyComponent implements OnInit {
+  isRouteLogin: boolean = false;
+
+  constructor(private location: Location, private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkCurrentRoute();
+  }
 
   backClicked() {
     this.location.back();
+  }
+
+  checkCurrentRoute(): void {
+    const currentUrl = this.router.url;
+    if (currentUrl === '/login/privacy-policy') {
+      this.isRouteLogin = true;
+    }
   }
 }
