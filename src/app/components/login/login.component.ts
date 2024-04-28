f0ff72a1b1b33dda8636829917ebfc03f3616269
Chneemann <Andre-Ke@gmx.de>
2024-04-28 13:41:38 +0200

    design login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { FormBtnComponent } from '../../shared/components/buttons/form-btn/form-btn.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, FormBtnComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginData = {
+    mail: '',
+    password: '',
+  };
 
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid) {
+    }
+  }
 }
