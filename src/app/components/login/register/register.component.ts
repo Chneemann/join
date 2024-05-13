84d007ce7fba29fdaa3405ed679676f0f8b31f62
Chneemann <Andre-Ke@gmx.de>
2024-05-13 04:33:45 +0200

    design register component

--- src/app/components/login/register/register.component.ts
+++ src/app/components/login/register/register.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormBtnComponent } from '../../../shared/components/buttons/form-btn/form-btn.component';
+import { FirebaseService } from '../../../services/firebase.service';
+import { LoginService } from '../../../services/login.service';
+import { SharedService } from '../../../services/shared.service';
+import { BtnBackComponent } from '../../../shared/components/buttons/btn-back/btn-back.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    FormBtnComponent,
+    BtnBackComponent,
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerData = {
+    name: '',
+    mail: '',
+    password: '',
+    passwordConfirm: '',
+  };
+
+  constructor(
+    private firebaseService: FirebaseService,
+    public loginSerivce: LoginService,
+    public sharedService: SharedService
+  ) {}
+
+  onSubmit(ngForm: NgForm) {
+    this.sharedService.isBtnDisabled = true;
+    if (ngForm.submitted && ngForm.form.valid) {
+      //this.loginSerivce.register(this.registerData);
+    }
+  }
+
+  checkIfUserEmailIsValid(emailValue: string) {
+    const channelNameLenght = emailValue.length;
+    const emailRegex = /^[\w.%+-]+@[\w.-]+\.[a-zA-Z]{2,}$/;
+    if (emailRegex.test(emailValue)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
