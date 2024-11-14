bf88b88b99429810106a74d408ea38a16f5dcb5c
Chneemann <andre-kempf@mailbox.org>
2024-11-14 04:42:26 +0100

    docs: add JSDoc comments to OverlayService

--- src/app/services/overlay.service.ts
+++ src/app/services/overlay.service.ts
@@ -10,6 +10,12 @@ export class OverlayService {
 
   constructor() {}
 
+  /**
+   * Set the overlay data subject.
+   *
+   * @param overlay The overlay type.
+   * @param data The overlay data.
+   */
   setOverlayData(overlay: string, data: any) {
     this.overlayDataSubject.next({ overlay, data });
   }
