44add1ee3d8f726912ce0971d40315dfe6ebb086
Chneemann <andre-kempf@mailbox.org>
2024-11-14 04:43:51 +0100

    docs: add JSDoc comments to LanguageService

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -11,6 +11,11 @@ export class LanguageService {
     this.translate.setDefaultLang(this.currentLang);
   }
 
+  /**
+   * Switches the language for the application.
+   *
+   * @param lang A string denoting the language to switch to, either 'en' or 'de'.
+   */
   switchLanguage(lang: string) {
     if (lang === 'en') {
       this.currentLang = 'en';
